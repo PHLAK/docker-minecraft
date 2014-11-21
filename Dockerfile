c1692d9d5cd5ca32ffbb64728319cf965de1b8bc
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-11-20 17:46:15 -0700

    Updated update.sh script to be more robust and allow updating to specific version numbers. Integrated update.sh script into the image provisioning process.

--- Dockerfile
+++ Dockerfile
@@ -5,20 +5,21 @@ MAINTAINER Chris Kankiewicz <Chris@ChrisKankiewicz.com>
 RUN apt-get update && apt-get -y upgrade
 
 ## Install Minecraft dependencies
-RUN apt-get -y install openjdk-7-jre-headless wget
+RUN apt-get -y install jq openjdk-7-jre-headless wget
 
 ## Create Minecraft directry
 RUN mkdir /srv/minecraft
 
-## Download and install the Minecraft server deb
-ADD https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar /srv/minecraft/minecraft_server.jar
-
-## Add the EULA file
-ADD files/eula.txt /srv/minecraft/eula.txt
 
 ## Add and chmod update script
 ADD files/update.sh /srv/minecraft/update.sh
 RUN chmod +x /srv/minecraft/update.sh
+
+## Download and install the Minecraft server deb
+RUN /srv/minecraft/update.sh
+
+## Add the EULA file
+ADD files/eula.txt /srv/minecraft/eula.txt
 
 ## Perform apt cleanup
 RUN apt-get -y autoremove && apt-get -y clean && apt-get -y autoclean
--- files/update.sh
+++ files/update.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env bash
 
 ## Require argument
-if [[ -z "${1}" ]]; then
-    echo "Usage: $(basename ${0}) [MINECRAFT_SERVER_JAR_URL]"; exit 1
+if [[ ! -z "${1}" ]]; then
+
+    ## Set version number
+    VERSION="${1}"
+
+else
+
+    ## Notify user of intent to update to latest version
+    echo "NOTICE: No version number specified, updating to latest version"
+
+    ## Set version to latest stable version
+    VERSION="$(wget -qO- https://s3.amazonaws.com/Minecraft.Download/versions/versions.json | jq -r '.latest.release')"
+
+fi
+
+## Set the server URL
+SERVER_URL="https://s3.amazonaws.com/Minecraft.Download/versions/${VERSION}/minecraft_server.${VERSION}.jar"
+
+## Check if file exists
+FILE_EXISTS="$(wget --spider -qO /dev/null ${SERVER_URL} && echo 'true' || echo 'false')"
+
+## Error on non-existent server file
+if [[ "${FILE_EXISTS}" == false ]]; then
+    echo "ERROR: Unable to locate server file for version ${VERSION}"; exit 1
 fi
 
 ## Fetch the server file
-wget -O /srv/minecraft/minecraft_server.update ${1} || exit 1
+wget --verbose -O /srv/minecraft/minecraft_server.update ${SERVER_URL} || exit 1
 
 ## Notify user on success
-echo "NOTICE: Updated server file has been downloaded. Restart the container to apply the update."
+echo "NOTICE: Updated server file has been downloaded. Restart the container to apply the update to version ${VERSION}"
