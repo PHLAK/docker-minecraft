608e9bd09e69b7c75587f7dfb266dacfcaf29d8b
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2015-08-09 15:51:51 -0700

    Added build.sh script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Set the image name
+IMAGE_NAME="phlak/minecraft"
+
+# Set script directory path
+SCRIPT_DIR="$(dirname $(readlink -f ${0}))"
+
+# Set tag from git commit hash
+TAG="$(git rev-parse --short HEAD)"
+
+# Build the image
+docker build --force-rm --pull --tag ${IMAGE_NAME}:${TAG} ${SCRIPT_DIR}
+
+# Notify user of success
+echo "Sucessfully created image: ${IMAGE_NAME}:${TAG}"
